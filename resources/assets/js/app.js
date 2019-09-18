b45ac2551b316553aea940c9daa6e44344c7797b
Buter <aleksbutav@gmail.com>
2019-09-19 00:34:39 +0300

    Added FeatureList component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,7 @@ import { populateAmenitiesAndPrices } from './helpers';
 
 import ImageCarousel from './components/ImageCarousel';
 import HeaderImage from './components/HeaderImage';
+import FeatureList from './components/FeatureList';
 import ModalWindow from './components/ModalWindow';
 
 let model = populateAmenitiesAndPrices(JSON.parse(window.vuebnb_listing_model));
@@ -22,6 +23,7 @@ var app = new Vue({
     },
     components: {
         ImageCarousel,
+        FeatureList,
         HeaderImage,
         ModalWindow
     }
